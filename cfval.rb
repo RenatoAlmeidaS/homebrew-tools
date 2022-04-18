ab402eb3982db08d59865925dc449de1b8a26852
RenatoAlmeidaS <Renatodealmeida_9@hotmail.com>
2022-04-18 11:16:50 -0300

    Removed :unneeded
    https://github.com/derailed/homebrew-popeye/pull/3/commits/4e1b15bd09beaaf770e6998ec3cb3c1b7530bdb2

--- cfval.rb
+++ cfval.rb
@@ -4,8 +4,6 @@ class Cfval < Formula
   url "https://github.com/jagregory/cfval/releases/download/v0.3.0/cfval-0.3.0.tar.gz"
   sha256 "1f1811970bba43a2c0956448db725384f59d3fe7fda2527f447505e57198280c"
 
-  bottle :unneeded
-
   def install
     bin.install "cfval"
   end
